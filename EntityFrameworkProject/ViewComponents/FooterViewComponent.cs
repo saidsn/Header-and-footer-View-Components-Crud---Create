0ca920361e6315baec5a934d54a97b36f677fafd
saidsn <saidsn@code.edu.az>
2022-10-18 18:45:21 +0400

    homework

---
+++ EntityFrameworkProject/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using EntityFrameworkProject.Data;
+using EntityFrameworkProject.Models;
+using EntityFrameworkProject.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkProject.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+
+        private readonly LayoutService _layoutService;
+        private readonly AppDbContext _appDbContext;
+
+        public FooterViewComponent(LayoutService layoutService)
+        {
+            _layoutService = layoutService;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult(View());
+        }
+
+
+    }
+}
